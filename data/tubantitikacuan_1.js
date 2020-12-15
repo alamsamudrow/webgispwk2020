1db8122661e33266a56238ee63b5e3fba8f5359b
alamsamudrow <76034571+alamsamudrow@users.noreply.github.com>
2020-12-15 19:57:37 +0700

    Add files via upload

---
+++ data/tubantitikacuan_1.js
@@ -0,0 +1,8 @@
+var json_tubantitikacuan_1 = {
+"type": "FeatureCollection",
+"name": "tubantitikacuan_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 112.045392208756354, -6.899190864064956 ] } }
+]
+}
